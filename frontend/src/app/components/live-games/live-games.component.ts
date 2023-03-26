379e752d2203f4e8e9d5ca75d6ac315e3b8b40d3
chris-ian-jones <chrisjones@Chriss-MacBook-Pro.local>
2023-03-26 18:28:26 +0100

    Added sortGames functionality to live-games tab

--- frontend/src/app/components/live-games/live-games.component.ts
+++ frontend/src/app/components/live-games/live-games.component.ts
@@ -38,4 +38,45 @@ export class LiveGamesComponent implements OnInit {
     return '';
   }
 
+  public sortGames(games: IGame[]): IGame[] {
+    const inGame: IGame[] = [],
+      ended: IGame[] = [],
+      aboutToStart: IGame[] = [];
+    
+      games.forEach(game => {
+        switch(this.getGameState(game)) {
+          case 'ended':  // if (x === 'value1')
+            ended.push(game);
+            break;
+          case 'about-to-start':  // if (x === 'value2')
+            aboutToStart.push(game);
+            break;
+          default:
+            inGame.push(game);
+            break;
+        }
+      });
+
+    inGame.sort((a, b) => {
+      return this.getGameMinute(b.startTime) - this.getGameMinute(a.startTime);
+    });
+  
+    ended.sort((a, b) => {
+      return new Date(b.endTime).getTime() - new Date(a.endTime).getTime();
+    });
+  
+    aboutToStart.sort((a, b) => {
+      return new Date(a.startTime).getTime() - new Date(b.startTime).getTime();
+    });
+  
+    return [...inGame, ...ended, ...aboutToStart];
+  }
+
+  getGameMinute(gameStartTime: string): number {
+    const start = new Date(gameStartTime),
+      now = new Date(),
+      diff = Math.floor((now.getTime() - start.getTime()) / 1000);
+    return Math.floor(diff / 60);
+  }
+
 }
